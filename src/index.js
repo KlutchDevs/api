6b32905f4e5f807b43d9da7c02dfeb20a1a2380a
Klutch <klutchdevs@gmail.com>
2022-03-29 20:38:00 -0500

    message update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 // index.js
 // This is the main entry point of our application
-const express = require('express');
-const app = express();
+const express = require('express'); //dependency
+const app = express(); //create app object
+
+/*We then use the app object’s get method to
+instruct our application to send a response of 
+“Hello World” when a user accesses the root URL (/). */
 
 app.get('/', (req, res) => res.send('Hello World'));
-app.listen(4000, () => console.log("Listening on port 4000!"));
+//instruct app to run on specific port
+app.listen(4000, () => console.log("Antonio's Listening on port 4000!"));
