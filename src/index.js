5f866a039ab141551e35baf75e062a4ba45a3e68
Klutch <klutchdevs@gmail.com>
2022-03-30 21:51:48 -0500

    Servers & Dynamic Port Hosting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 // index.js
 // This is the main entry point of our application
 const express = require('express'); //dependency
+
 const app = express(); //create app object
+const port = process.env.PORT || 4000;
 
 /*We then use the app object’s get method to
 instruct our application to send a response of 
 “Hello World” when a user accesses the root URL (/). */
 
-app.get('/', (req, res) => res.send("Hello World, I'm Just Getting Started. -Klutch"));
+app.get('/', (req, res) => res.send("Lets see if nodemon is working. -Klutch"));
 //instruct app to run on specific port
-app.listen(4000, () => console.log("Antonio's Listening on port 4000!"));
+app.listen(port, () => 
+    console.log("Server running at http://localhost:${port}")
+);
